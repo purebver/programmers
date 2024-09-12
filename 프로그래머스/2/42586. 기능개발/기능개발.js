efc3d98ad7ad0132a4f73cd63709a04195039bde
purebver <wkddj1541@gmail.com>
2024-09-12 09:56:58 +0900

    [level 2] Title: 기능개발, Time: 0.24 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,31 @@
+function solution(progresses, speeds) {
+    let count = 0;
+    const cal = (num, i) => {
+        while(num < 100){
+            num += speeds[i];
+            count++;
+        }
+    }
+    const result = [];
+    let result_count = 0;
+    for(let i = 0; i < progresses.length; i++){
+        let bool = true;
+        const sum = progresses[i] + speeds[i] * count;
+        
+        if(sum < 100){
+            cal(sum, i);
+            bool = false;
+        }
+        if(bool){
+            result_count++;
+        } else if (result_count !== 0){
+            result.push(result_count);
+            result_count = 1;
+        } else {
+            result_count++;
+        }
+    }
+    result.push(result_count);
+    return result;
+    
+}
